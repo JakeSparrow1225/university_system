695783ac93046c4c62983d283af9b318cbb9a645
JakeSparrow1225 <108857007+JakeSparrow1225@users.noreply.github.com>
2024-06-07 13:39:21 +0900

    Update slack_try_0.py
    30秒おきに過去のメッセージを収集し，一度収集したデータを２度収集しないように，過去の定期実行のメッセージを収集しないようにした．

--- slack_try_0.py
+++ slack_try_0.py
@@ -5,12 +5,24 @@ import time
 SLACK_ACCESS_TOKEN = ''
 CHANNEL_ID = ''
 
+# 前回の収集結果を保持する変数
+last_messages = []
+
 def get_messages():
+    global last_messages  # 前回の収集結果を更新するためにglobal宣言
+
     client = WebClient(SLACK_ACCESS_TOKEN)
     response = client.conversations_history(channel=CHANNEL_ID)
     messages = response.get('messages')
 
+    new_messages = []  # 新しく収集したメッセージを保持するリスト
+
     for message in messages:
+        if message not in last_messages:
+            new_messages.append(message)
+            last_messages.append(message)
+
+    for message in new_messages:
         channel = message.get('channel')
         user = message.get('user')
         text = message.get('text')
