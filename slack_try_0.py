0814338310c578822e30592ff5206a47aed3c92c
JakeSparrow1225 <108857007+JakeSparrow1225@users.noreply.github.com>
2024-06-07 13:15:11 +0900

    Update slack_try_0.py
    定期実行が初めて完成

--- slack_try_0.py
+++ slack_try_0.py
@@ -1,22 +1,25 @@
-# https://gri.jp/media/entry/7678 サイト
 from slack_sdk import WebClient
+import schedule
+import time
 
-#slackのbotを作成して，チャンネルに追加する．
-#その際に得られるトークンとチャンネルＩＤを書いてる
 SLACK_ACCESS_TOKEN = ''
 CHANNEL_ID = ''
 
-#Slack APIにアクセスするためのWebClientオブジェクトを作成
-#clientオブジェクトを使用して，SlackAPIのconversations.historyメソッドを呼び出し
-client = WebClient(SLACK_ACCESS_TOKEN)
+def get_messages():
+    client = WebClient(SLACK_ACCESS_TOKEN)
+    response = client.conversations_history(channel=CHANNEL_ID)
+    messages = response.get('messages')
 
-#responseにはAPIの応答が格納される
-response = client.conversations_history(channel=CHANNEL_ID)
+    for message in messages:
+        channel = message.get('channel')
+        user = message.get('user')
+        text = message.get('text')
+        print(f"チャンネル: {channel}, ユーザ名: {user}, 発言内容: {text}")
 
-messages = response.get('messages')
+# 30秒ごとにget_messages関数を実行するスケジュールを設定
+schedule.every(30).seconds.do(get_messages)
 
-for message in messages:
-    channel = message.get('channel')
-    user = message.get('user')
-    text = message.get('text')
-    print(f"チャンネル: {channel}, ユーザ名: {user}, 発言内容: {text}")
+# 無限ループでスケジュールを実行し続ける
+while True:
+    schedule.run_pending()
+    time.sleep(1)
